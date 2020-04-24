faf2a1cd2d61ee211963372544cab6b04fe77c35
demdecuong <nguyenphucminh2804@gmail.com>
2020-04-24 23:23:35 +0700

    open-release entry point

--- src/randNumber/randNum_chardev.c
+++ src/randNumber/randNum_chardev.c
@@ -20,13 +20,18 @@
 // static dev_t first; // Global variable for the first device number
 // static struct cdev c_dev; // Global variable for the character device structure
 // static struct class *cl; // Global variable for the device class
-
+typedef struct vchar_dev{
+	unsigned char * control_regs;
+	unsigned char * status_regs;
+	unsigned char * data_regs;
+}vchar_dev_t;
 struct _vchar_drv
 {
 	dev_t dev_num;
 	struct class *dev_class;
 	struct device *dev;
 	struct cdev * vcdev;
+	vchar_dev_t * vchar_hw;
 	unsigned int open_cnt;
 }vchar_drv;
 
@@ -36,9 +41,9 @@ static int rVal;
 /******************* device specific - START ***********************/
 // Ham khoi tao thiet bi
 // Ham giai phong thiet bi
-// void vchar_hw_exit(vchar_dev_t *hw){
-// 	kfree(hw->control_regs);
-// }
+void vchar_hw_exit(vchar_dev_t *hw){
+	kfree(hw->control_regs);
+}
 // Ham doc tu cac thanh ghi du lieu cua thiet bi
 // Ham ghi vao cac thanh ghi dieu khien cua thiet bi    
 // Ham xu ly tin hieu gui tu thiet bi
@@ -99,8 +104,8 @@ static struct file_operations fops =
         .owner = THIS_MODULE,
         .open = dev_open,
         .release = dev_close,
-        .read = dev_read,
-        .write = dev_write
+        //.read = dev_read,
+        //.write = dev_write,
 };
 
 static int __init init_chardev(void) /* Driver Constructor */
@@ -123,14 +128,14 @@ static int __init init_chardev(void) /* Driver Constructor */
 	{
 		printk("failed to create a device class\n");
         unregister_chrdev_region(vchar_drv.dev_num,1);
-        return -1;
+       // return -1;
 	}
 	vchar_drv.dev = device_create(vchar_drv.dev_class, NULL,vchar_drv.dev_num, NULL,"random_dev");
 	if( IS_ERR(vchar_drv.dev))
 	{
 		printk("failed to create a device\n");
         class_destroy(vchar_drv.dev_class);
-        return -1;
+        //return -1;
 	}
 
 	//Cap phat bo nho cho cac ctdl cua driver va khoi tao
@@ -147,16 +152,22 @@ static int __init init_chardev(void) /* Driver Constructor */
 	// }
 
 	//Dang ky cac entry point voi kernel - Entry points register
+	vchar_drv.vcdev = cdev_alloc();
+	if( vchar_drv.vcdev == NULL){
+		printk('failed to allocate cdev strucure');
+		vchar_hw_exit(vchar_drv.vchar_hw);
+	}
 
-	// cdev_init(vchar_drv.vcdev, &fops);
-	// ret = cdev_add(vchar_drv.vcdev, vchar_drv.dev_num,1);
-	// if(ret < 0){
-	// 	printk("failed to add character device to system\n");
-	// 	device_destroy(vchar_drv.dev_class,vchar_drv.dev_num);
-	// 	class_destroy(vchar_drv.dev_class);
-	// 	unregister_chrdev_region(vchar_drv.dev_num,1);
-	// 	return -1;
-	// }
+	cdev_init(vchar_drv.vcdev, &fops);
+	ret = cdev_add(vchar_drv.vcdev, vchar_drv.dev_num,1);
+	if(ret < 0){
+		printk("failed to add character device to system\n");
+		vchar_hw_exit(vchar_drv.vchar_hw);
+		// device_destroy(vchar_drv.dev_class,vchar_drv.dev_num);
+		// class_destroy(vchar_drv.dev_class);
+		// unregister_chrdev_region(vchar_drv.dev_num,1);
+		//return -1;
+	}
 	// Dang ky ham xu ly ngat
 
 	printk("Initialize character driver successfully\n");
