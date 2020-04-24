dc051e28ae2e291c21094e9cf433a54ea1ddd153
demdecuong <nguyenphucminh2804@gmail.com>
2020-04-25 00:44:17 +0700

    clean file

--- src/randNumber/.randNum_chardev.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/minh/Desktop/Project_OS/src/randNumber/randNum_chardev.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/minh/Desktop/Project_OS/src/randNumber/randNum_chardev.ko /home/minh/Desktop/Project_OS/src/randNumber/randNum_chardev.o /home/minh/Desktop/Project_OS/src/randNumber/randNum_chardev.mod.o ;  true
