a5d3732fefae5c7a2d584a0324c3be494aabb282
Huy learns to code <39640834+lqhuyapcs@users.noreply.github.com>
2020-04-26 08:52:20 -0400

    Update randNum_chardev.c

--- src/randNumber/randNum_chardev.c
+++ src/randNumber/randNum_chardev.c
@@ -1,4 +1,3 @@
-
 // #include <linux/version.h>
 // #include <linux/kernel.h>
 // #include <linux/types.h>
@@ -17,9 +16,6 @@
 #define DRIVER_VERSION "1.0"
 
 
-// static dev_t first; // Global variable for the first device number
-// static struct cdev c_dev; // Global variable for the character device structure
-// static struct class *cl; // Global variable for the device class
 typedef struct vchar_dev{
 	unsigned char * control_regs;
 	unsigned char * status_regs;
@@ -59,16 +55,12 @@ static int dev_open(struct inode* i, struct file *f)
     vchar_drv.open_cnt++;
 	printk("Handle opened event (%d)\n",vchar_drv.open_cnt);
 	return 0;
-    // printk(KERN_INFO "Driver: open()\n");
-    // return 0;
 }
 
 static int dev_close(struct inode *i, struct file *f)
 {
     printk("Handle closed event\n");
 	return 0;
-    // printk(KERN_INFO "Driver: close()\n");
-    // return 0;
 }
 static ssize_t dev_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
@@ -76,7 +68,6 @@ static ssize_t dev_read(struct file *f, char __user *buf, size_t len, loff_t *of
        
     printk("Reading event...\n");
     get_random_bytes(&rVal, 4);
-    //rVal = initRandomInteger();
     
     /* You have just a single char in your buffer, so only 0 offset is valid */
     if (*off > 0) return 0; /* End of file */
@@ -89,14 +80,6 @@ static ssize_t dev_read(struct file *f, char __user *buf, size_t len, loff_t *of
 static ssize_t dev_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
     printk("Writing event...\n");
-    // if (copy_from_user(&c, buf + len - 1, 1) != 0)
-    // {
-    //     return -EFAULT;
-    // }
-    //return len;
-    //int num_bytes = 4;
-    // *off += 4;
-	// return 4;
     return len;
 }
 static struct file_operations fops =
@@ -128,28 +111,14 @@ static int __init init_chardev(void) /* Driver Constructor */
 	{
 		printk("failed to create a device class\n");
         unregister_chrdev_region(vchar_drv.dev_num,1);
-       // return -1;
 	}
 	vchar_drv.dev = device_create(vchar_drv.dev_class, NULL,vchar_drv.dev_num, NULL,"random_dev");
 	if( IS_ERR(vchar_drv.dev))
 	{
 		printk("failed to create a device\n");
         class_destroy(vchar_drv.dev_class);
-        //return -1;
 	}
 
-	//Cap phat bo nho cho cac ctdl cua driver va khoi tao
-	// vchar_drv.vchar_hw = kzalloc(sizeof(vchar_dev_t), GFP_KERNEL);
-	// if(!vchar_drv.vchar_hw){
-	// 	printk("failed to allocated data structure of the driver\n");
-	// 	ret = -ENOMEM;
-	// 	goto failed_allocate_structure;
-	// }
-	// vchar_drv.vcdev = cdev_alloc();
-	// if(vchar_drv.vcdev == NULL){
-	// 	printk("failed to allocate cdev structure\n");
-	// 	goto failed_allocate_cdev;
-	// }
 
 	//Dang ky cac entry point voi kernel - Entry points register
 	vchar_drv.vcdev = cdev_alloc();
@@ -163,32 +132,16 @@ static int __init init_chardev(void) /* Driver Constructor */
 	if(ret < 0){
 		printk("failed to add character device to system\n");
 		vchar_hw_exit(vchar_drv.vchar_hw);
-		// device_destroy(vchar_drv.dev_class,vchar_drv.dev_num);
-		// class_destroy(vchar_drv.dev_class);
-		// unregister_chrdev_region(vchar_drv.dev_num,1);
-		//return -1;
 	}
 	// Dang ky ham xu ly ngat
 
 	printk("Initialize character driver successfully\n");
 	return 0;
 
-
-
-// failed_init_hw:
-// 	kfree(vchar_drv.vchar_hw);
-
 }
 
 static void __exit exit_chardev(void) /* Destructor */
 {
-	// Huy dang ky entry point voi kernel
-	//cdev_del(vchar_drv.vcdev);
-
-	// Giai phong thiet bi vat ly
-	// vchar_hw_exit(vchar_drv.vchar_hw);
-	// // Giai phong bo nho da cap phat ctdl cua driver
-	// kfree(vchar_drv.vchar_hw);
 	// Giai phong device file
 	device_destroy(vchar_drv.dev_class, vchar_drv.dev_num);
 	class_destroy(vchar_drv.dev_class);
